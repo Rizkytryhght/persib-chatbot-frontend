a8b9253fa47ea95c853b82689d678ce6d4784b96
Moch Rizky Taufiqurrahman <moch.rizky.tif23@polban.ac.id>
2026-06-02 05:11:24 +0700

    initial setup route untuk landingpage, profile, setting. -rizky

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,6 @@
+// Landing.jsx
+function LandingPage() {
+  return <h1>Landing Page</h1>;
+}
+
+export default LandingPage;
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,6 @@
+// Landing.jsx
+function Profile() {
+  return <h1>Profile Page</h1>;
+}
+
+export default Profile;
