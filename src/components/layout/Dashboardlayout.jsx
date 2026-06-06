a0a42951045261be27467cc12344876521730e35
Moch Rizky Taufiqurrahman <moch.rizky.tif23@polban.ac.id>
2026-06-07 02:20:17 +0700

    update sidebar ui/ux. -rizky

--- src/components/layout/Dashboardlayout.jsx
+++ src/components/layout/Dashboardlayout.jsx
@@ -1,24 +1,17 @@
 import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
-
 import "../../styles/dashboard.css";
 
 function DashboardLayout({ children }) {
   return (
     <div className="dashboard-layout">
-
       <Sidebar />
-
       <div className="dashboard-content">
-
         <Topbar />
-
         <main className="dashboard-main">
           {children}
         </main>
-
       </div>
-
     </div>
   );
 }
