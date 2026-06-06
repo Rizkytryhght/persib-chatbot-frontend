640905c89a76d5c315a1fcd0a13e9bf31ea67f9a
Moch Rizky Taufiqurrahman <moch.rizky.tif23@polban.ac.id>
2026-06-07 06:01:06 +0700

    update topbar. -rizky

--- src/components/layout/Topbar.jsx
+++ src/components/layout/Topbar.jsx
@@ -1,46 +1,31 @@
 import "../../styles/topbar.css";
 import userData from "../../data/userData";
 import { Link } from "react-router-dom";
+import profileImage from "../../assets/images/fotoDefault.jpg"
 
 function Topbar() {
   return (
     <header className="topbar">
-
       <div className="topbar-right">
+        <button className="topbar-icon">💬</button>
+        <button className="topbar-icon">🛡️</button>
 
-        <button className="topbar-icon">
-          💬
-        </button>
-
-        <button className="topbar-icon">
-          🛡️
-        </button>
-
-        <Link
-          to="/profile"
-          className="profile-link"
-        >
-
+        <Link to="/profile" className="profile-link">
           <div className="user-info">
-
-            <div className="user-name">
-              {userData.name}
-            </div>
-
-            <div className="user-role">
-              {userData.role}
-            </div>
-
+            <div className="user-name">{userData.name}</div>
+            <div className="user-role">{userData.role}</div>
           </div>
-
+          
+          {/* 2. Ganti div user-avatar lama dengan tag img */}
           <div className="user-avatar">
-            BS
+            <img 
+              src={profileImage} 
+              alt={userData.name} 
+              className="avatar-image-topbar" 
+            />
           </div>
-
         </Link>
-
       </div>
-
     </header>
   );
 }
